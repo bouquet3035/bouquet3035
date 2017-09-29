79d4e0ed5b52046dfa90b317b5d0911aad2a4f80
bouquet <bouquet3035@gmail.com>
2017-09-29 15:31:19 +0900

    블런치 test에 출력 코드 추가

--- Team/src/HelloWorld.java
+++ Team/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Time is 1:06 AM");
+		System.out.println("Soooooo hot!!");
 	}
 
 }
