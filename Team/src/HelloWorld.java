bee3c1d257b2b9f3f4e8232369646d31160f783a
bouquet <bouquet3035@gmail.com>
2017-09-29 15:30:12 +0900

    출력문구추가

--- Team/src/HelloWorld.java
+++ Team/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Time is 1:06 AM");
+		System.out.println("¾Æ¸§´Ù¿î ¹äÀÌ¾ä");
 	}
 
 }
